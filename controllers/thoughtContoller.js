72af3fc2627766e9e2880c22f453a3cfe0e687ba
brandon6457 <brandon6457@gmail.com>
2023-06-11 15:12:02 -0400

    finished thoughts

--- controllers/thoughtContoller.js
+++ controllers/thoughtContoller.js
@@ -51,3 +51,83 @@ module.exports = {
      return res.status(500).json(err);
    }
  },
+  
+  async deleteThought(req, res) {
+   try {
+     const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+     if (!thought) {
+       return res.status(404).json({ message: 'No thought with that ID' });
+     }
+
+     const users = await User.updateMany({},
+       {
+         $pull: {
+           thoughts: req.params.thoughtId
+         }
+       })
+
+     res.json({ message: 'Thought deleted!', users });
+   } catch (err) {
+     res.status(500).json(err);
+   }
+ },
+ 
+ async updateThought(req, res) {
+   try {
+     const thought = await Thought.findOneAndUpdate(
+       { _id: req.params.thoughtId },
+       { $set: req.body },
+       { runValidators: true, new: true }
+     );
+
+     if (!thought) {
+       return res.status(404).json({ message: 'No thought with this id!' });
+     }
+
+     res.json(thought);
+   } catch (err) {
+     res.status(500).json(err);
+   }
+ },
+ 
+ async addReaction(req, res) {
+   try {
+     const thought = await Thought.findOneAndUpdate(
+       { _id: req.params.thoughtId },
+       { $addToSet: { reactions: req.body } },
+       { runValidators: true, new: true }
+     );
+
+     if (!thought) {
+       return res
+         .status(404)
+         .json({ message: 'No thought found with that ID :(' })
+     }
+
+     res.json(thought);
+   } catch (err) {
+     res.status(500).json(err);
+   }
+ },
+ 
+ async removeReaction(req, res) {
+   try {
+     const thought = await Thought.findOneAndUpdate(
+       { _id: req.params.thoughtId },
+       { $pull: { reactions: { reactionId: req.params.reactionId } } },
+       { runValidators: true, new: true }
+     );
+
+     if (!thought) {
+       return res
+         .status(404)
+         .json({ message: 'No thought found with that ID' });
+     }
+
+     res.json(thought);
+   } catch (err) {
+     res.status(500).json(err);
+   }
+ },
+};
